e7d8a0f8dad874f79228849250dcde246eff8029
Sam <53nicholsonsam@gmail.com>
2020-05-13 11:51:09 -0500

    Now have a script that displays an image for 3 sec, just need to write it into a function

--- src/UI.py
+++ src/UI.py
@@ -18,3 +18,34 @@ for i in range(0,len(deck)):
     cardidentifier[deck[i]]=deckforimg[i]
 print(cardidentifier)
 pygame.init()
+
+# define the RGB value 
+# for white colour 
+white = (255, 255, 255) 
+  
+# assigning values to X and Y variable 
+X = 50
+Y = 50
+  
+# create the display surface object 
+# of specific dimension..e(X, Y). 
+display_surface = pygame.display.set_mode((X, Y )) 
+display_width = 800
+display_height = 600
+# set the pygame window name 
+pygame.display.set_caption('Image')
+image = pygame.image.load('src/imgs/2C.png').convert()
+image=pygame.transform.rotozoom(image, 0, .5)
+x = 20 # x coordnate of image
+y = 30 # y coordinate of image
+
+ 
+running = True
+while (running): # loop listening for end of game
+    screen = pygame.display.set_mode((display_width, display_height ))
+    screen.blit(image, ( x,y)) # paint to screen
+    pygame.display.flip() # paint screen one time
+    pygame.time.wait(3000)
+    running=False
+#loop over, quit pygame
+pygame.quit()
